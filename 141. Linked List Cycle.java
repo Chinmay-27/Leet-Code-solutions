355e2255a28f2c1c4bf9a22e5a2f4c72fdd82c79
Chinmay Raut <68195400+Chinmay-27@users.noreply.github.com>
2024-03-13 23:21:30 +0530

    Create 141. Linked List Cycle.java

---
+++ 141. Linked List Cycle.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
