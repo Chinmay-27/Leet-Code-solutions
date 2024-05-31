a48a5f53381963551cb7c50ed3b0f003a0ad2622
Chinmay-27 <68195400+Chinmay-27@users.noreply.github.com>
2024-05-31 22:20:28 +0530

    Update 169. Majority Element.java

--- Pratice Questions/169. Majority Element.java
+++ Pratice Questions/169. Majority Element.java
@@ -1,22 +1,27 @@
-class Solution {
+public class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
-        int count  = 0;
+        int count = 0;
         int ele = nums[0];
-        
-        for (int i = 0; i < nums.length; i++) {
-            if(count == 0){
+
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
                 ele = nums[i];
             }
-            else if(nums[i] == ele){
+            if (nums[i] == ele) {
                 count++;
+            } else {
+                count--;
             }
-            else count--;
         }
+
+        // Verify that the element is the majority element
         int m = 0;
         for (int i : nums) {
-            if(i == ele) m++;
+            if (i == ele) {
+                m++;
+            }
         }
-        return (m > n/2)? ele:-1;
+        return (m > n / 2) ? ele : -1;
     }
 }
