3ca2f6b74b4719cd7b883930d97499bdde8971fa
Chinmay-27 <68195400+Chinmay-27@users.noreply.github.com>
2024-06-04 23:24:44 +0530

    Create 206. Reverse Linked List.java

---
+++ Pratice Questions/206. Reverse Linked List.java
@@ -0,0 +1,33 @@
+
+class ListNode {
+    int val;
+    ListNode head;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode node = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = node;
+            node = head;
+            head = temp;
+        }
+
+        return node;
+    }
+}
